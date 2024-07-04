bd0c52b9abc420ad96f0051d04829672a2e3a160
veena <veenasardana1@gmail.com>
2024-07-04 19:34:13 +0530

    first commit

---
+++ src/app/index.js
@@ -0,0 +1,11 @@
+import styles from './app.module.scss'
+function App() {
+  return (
+   <main className={styles.container}>
+    <h1>hello react</h1>
+   </main>
+   
+  );
+}
+
+export default App;
